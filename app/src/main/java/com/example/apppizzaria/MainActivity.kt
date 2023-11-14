f8333288de187b6b2f75c803d6c3dbfdf746715e
Projeto299 <desenvolvimento@299imports.com.br>
2023-11-14 15:22:03 -0300

    1.0

--- app/src/main/java/com/example/apppizzaria/MainActivity.kt
+++ app/src/main/java/com/example/apppizzaria/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.apppizzaria
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.apppizzaria.databinding.ActivityMainBinding
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    var clicked = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btAll.setOnClickListener {
+            clicked = true
+            if (clicked){
+                binding.btAll
+            }
+            }
+        }
+
+
     }
-}
